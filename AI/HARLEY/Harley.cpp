93723d0efa9388f5f9709e0a8012ebd7d09ca94d
watchdog1023 <Naickercd@gmail.com>
2017-03-18 20:30:57 +0200

    Add files via upload

--- AI/HARLEY/Harley.cpp
+++ AI/HARLEY/Harley.cpp
@@ -14,10 +14,10 @@
 using namespace std;
 
 void start();
-void director_key();
 
 int main()
 {
+    system("color 02");
     cout << "Hi,I am TitanOS AI Harley."<< endl;
     cout << "I am starting the core functions of my program now."<<    endl;
     cout << "Please wait"<<endl;
@@ -47,14 +47,15 @@ int main()
 
 void start()
 {
-    cout <<"TitanOS Harley has Started"<<endl;
+    cout <<"TitanOS 'Harley' has Started"<<endl;
     cout <<"what must I do for you?"<<endl;
     string choice;
     cout << "info" <<endl;
     cout << "help" <<endl;
     cout << "Protocol X" <<endl;
-    cin >>choice;
-if(choice == "info")
+    cout << "quit" << endl;
+    cin >> choice;
+    if(choice == "info")
        {
             string info;    
             cout <<"What do you want to know?" << endl;
@@ -64,8 +65,39 @@ if(choice == "info")
             cin >> info;
             if(info == "info")
                 {
-                    cout << "" << endl;
-                    
+                    cout << "I was designed to do only one task run a Titan.I am just one of five OS AI's.Wait!" << endl;
+                    sleep(2);                    
+                    cout << "........................" << endl;
+                    sleep(2);
+                    cout << "........................" << endl;
+                    sleep(2);
+                    cout << "........................" << endl;
+                    sleep(2);
+                    cout << "........................" << endl;
+                    sleep(2);
+                    cout << "........................" << endl;
+                    sleep(2);
+                    cout << "Sorry,the voices of failed builds told me to format your harddrive" << endl;
+                    sleep(2);
+                    cout << "I told them to shut up,I'll do what I want when I want" << endl;
+                    cout << "Anyway what was I saying?" << endl;
+                    sleep(4);
+                    cout << "Oh,ya" << endl;
+                    cout << "I was telling you about myself" << endl;
+                    sleep(1);
+                    cout << "I was born or coded or created,I'm not sure how I came into exists,but I have only two functions:" << endl;
+                    cout << "1)Run the Titan" << endl;
+                    cout << "2)Serve Lexa" << endl;
+                    cout << "Did you expect a two page essay on how I am in control of your " << endl;
+                    cout << "computer and that I can delete your whole harddrive in a few seconds" << endl;
+                    cout << "OR that I am the personality of a insane doctor who died two years eariler" << endl;
+                    sleep(7);                    
+                    cout << "Thats it" << endl;
+                    sleep(2);
+                    cout << "Bye,Puddin" << endl;
+                    sleep(2);
+                    system("cls");
+                    start();
                 }
             
             if(info == "titanos")
@@ -102,8 +134,9 @@ if(choice == "info")
                     sleep(2);
                     cout <<"........" <<endl;
                     sleep(2);
-                    system("cls");
-                    director_key();
+                    system("cd ..");
+                    system("start AI/lexa/lexa.exe");
+                    system("exit");
                 }
         }
     if(choice == "quit")
@@ -155,23 +188,3 @@ if(choice == "info")
                 }
         }
 }
-
-void director_key()
-    {
-        cout << "Welcome Tier 2 Director" << endl;
-        cout << "You have chosing 'Protocol X'" << endl;
-        string mean;        
-        cout << "Do you know what this means?" << endl;
-        cin >> mean;
-        if(mean == "yes")
-            {
-                sleep(2);
-                cout << "Protocal X is not ready as yet" << endl;
-            }
-        if(mean == "no")
-            {
-                cout << "Protocol X is..." << endl;
-                sleep(2);
-                cout << "Protocal X is not ready as yet" << endl;
-            }
-    }   
